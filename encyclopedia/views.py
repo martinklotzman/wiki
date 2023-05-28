6331d44401f15abffe7214285c02de8154109868
martin-ivorycc <martin.klotzman@ivorycc.com>
2023-05-27 22:22:50 -0700

    update terms and wiki list on index.html link to right wiki

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,11 +8,11 @@ def index(request):
         "entries": util.list_entries()
     })
     
-def entry(request, title):
+def wiki(request, title):
     # Retrieve the wiki entry using get_entry
-    entry = util.get_entry(title)
+    wiki = util.get_entry(title)
     
-    if entry is None:
+    if wiki is None:
         # If the entry does not exist, show an error message
         return render(request, "encyclopedia/error.html", {
             "error_message": f"Sorry, the entry <b>{title}</b> does not exist."
@@ -20,7 +20,7 @@ def entry(request, title):
         
     else:
         # If the entry exists, convert it from markdown to HTML and display
-        return render(request, "encyclopedia/entry.html", {
+        return render(request, "encyclopedia/wiki.html", {
             "title": title,
-            "entry": entry
+            "wiki": wiki
         })
