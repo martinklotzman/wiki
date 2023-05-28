1cd1af05b23e1911bd6d99a744bd7194ea8439bf
martin-ivorycc <martin.klotzman@ivorycc.com>
2023-05-27 21:53:31 -0700

    view.py calls util.py function to display wiki search entry. displays error for non-exist entry. created associated html. updated urls.py

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -7,4 +7,20 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
-
+    
+def entry(request, title):
+    # Retrieve the wiki entry using get_entry
+    entry = util.get_entry(title)
+    
+    if entry is None:
+        # If the entry does not exist, show an error message
+        return render(request, "encyclopedia/error.html", {
+            "error_message": f"Sorry, the entry <b>{title}</b> does not exist."
+        })
+        
+    else:
+        # If the entry exists, convert it from markdown to HTML and display
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "entry": entry
+        })
