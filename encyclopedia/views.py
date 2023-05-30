618c4f06cf327666470a90daeef6a27b2dca7a95
martin-ivorycc <martin.klotzman@ivorycc.com>
2023-05-30 10:13:32 -0700

    update urls.py, layout.html, and views.py to generate results for entry that exists

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from . import util
 
@@ -23,4 +23,21 @@ def wiki(request, title):
         return render(request, "encyclopedia/wiki.html", {
             "title": title,
             "wiki": wiki
+        })
+        
+def search(request):
+    # Get the search query from the GET data
+    query = request.GET.get("q", "").lower()
+    
+    # Get the list of all entries
+    entries = util.list_entries()
+    
+    # Direct user to wiki page if search result exists, else list related search results
+    if query in (entry.lower() for entry in entries):
+        return redirect('wiki', title=query)
+    else:
+        matching_entries = [entry for entry in entries if query in entry.lower()]
+        return render(request, "encyclopedia/search_results.html", {
+            "entries": matching_entries,
+            "query": query
         })
