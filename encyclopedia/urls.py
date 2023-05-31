861204c25940ab7ba351ef5ec8aaeb56de700ba5
martin-ivorycc <martin.klotzman@ivorycc.com>
2023-05-30 18:12:48 -0700

    ability to create a new wiki entry

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.wiki, name="wiki"),
     path("search/", views.search, name="search"),
+    path("entry/", views.entry, name="entry"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
-
+from django import forms
 from . import util
 
+class NewEntryForm(forms.Form):
+    title = forms.CharField(label="Title")
+    content = forms.CharField(widget=forms.Textarea, label="Content")
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -41,3 +44,31 @@ def search(request):
             "entries": matching_entries,
             "query": query
         })
+        
+def entry(request):
+    # If this is a POST request, try to process the form data
+    if request.method == "POST":
+        form = NewEntryForm(request.POST)
+        # Check if form is valid
+        if form.is_valid():
+            # Extract title and content from the form
+            title = form.cleaned_data["title"]
+            content = form.cleaned_data["content"]
+            # Check if an entry with this title exists
+            if util.get_entry(title) is not None:
+                # If the entry exists, render the form again with an error message
+                return render(request, "encyclopedia/new_entry.html", {
+                    "form": form,
+                    "existing_entry": True
+                })
+            # Save the new entry
+            util.save_entry(title, content)
+            # Redirect to the newly created entry page
+            return redirect('wiki', title=title)
+    # If this is a GET request or the form is invalid, render a new form
+    else:
+        form = NewEntryForm()
+    return render(request, "encyclopedia/new_entry.html", {
+        "form": form
+    })
+
